b21647112647efd25b4b4d95d305fe53d0a7f70d
tkulkarni13 <tejas.pie@gmail.com>
2024-04-30 02:12:29 -0700

    added showOptions function, and clarified comments

--- To-Do-List-Application.py
+++ To-Do-List-Application.py
@@ -1,11 +1,11 @@
-tasks = {}
+tasks = {} # Dictionary to store all tasks in to-do list
 
 def addTask(task): # Function to add task to to-do list. Every task starts as 'false' or 'incomplete'
     tasks[task] = False
 
 def viewTasks(): # Function to view all tasks on to-do list
     if (len(tasks) == 0):
-        print("You have no tasks on your to-do list") # If there are no tasks, notify user
+        print("You have no tasks on your to-do list") # If there are no tasks on the list, notify user
     else:
         for i, task in enumerate(tasks): # Loop to print all tasks
             if (tasks[task]): 
@@ -14,9 +14,9 @@ def viewTasks(): # Function to view all tasks on to-do list
                 print("{}. {}: Incomplete".format(i+1, task)) # Print 'Incomplete' for all other tasks
 
 def markTaskAsComplete(task): # Function to mark tasks as complete
-    try:
+    if (task in tasks):
         tasks[task] = True # Setting a task to 'true', marks it as complete
-    except KeyError: 
+    else:
         print("There is no such task on the to-do list") # If the task is not detected, notify user
 
 def deleteTask(task): # Function to delete a task
@@ -25,14 +25,18 @@ def deleteTask(task): # Function to delete a task
     except KeyError: 
         print("There is no such task on the to-do list") # If the task is not detected, notify user
 
-if __name__ == "__main__": # Main method for the command line interface, and calling above functions
-    # Print selection options for user
+def showOptions(): # Function to show selection options for the user
     print("Menu:")
     print("1. Add a task")
     print("2. View tasks")
     print("3. Mark a task as complete")
     print("4. Delete a task")
     print("5. Quit")
+    print("6. Show options")
+
+if __name__ == "__main__": # Main method for the command line interface, and calling above functions
+    print("Welcome to the To-Do List App!") # Print an intro for the user
+    showOptions() # Print selection options for user
 
     while True: # Loop until terminated by user
         try:
@@ -40,7 +44,7 @@ if __name__ == "__main__": # Main method for the command line interface, and cal
         except ValueError: 
             print("Please select a valid number") # If an invalid input is supplied by user (anything not an integer), notify user
         else:
-            if (user_num_input < 1 or user_num_input > 5): 
+            if (user_num_input < 1 or user_num_input > 6): 
                 print("Please input a number from 1-5") # If an invalid integar is inputted, notify user
 
             elif (user_num_input == 1): 
@@ -60,3 +64,6 @@ if __name__ == "__main__": # Main method for the command line interface, and cal
 
             elif (user_num_input == 5): 
                 break # Exit loop
+
+            elif (user_num_input == 6):
+                showOptions() # Print selection options for user
